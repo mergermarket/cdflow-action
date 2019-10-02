1de9f41db7188dcaef93d736ed47bde946cd86c2
Marcio Goda <marciogoda@gmail.com>
2019-10-02 17:33:10 +0100

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ try {
     child_process.execSync('docker pull mergermarket/cdflow-commands:latest');
     child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py');
     child_process.execSync('chmod +x ./cdflow ')
+    child_process.execSync('sudo mv ./cdflow /usr/local/bin/cdflow')
     //core.exportVariable('CDFLOW_IMAGE_ID','mergermarket/cdflow-commands')
 }catch {
 
