192e4c5e17ee9a0481e59b9b7edfbacfd6bfabfc
Marcio Goda <marciogoda@gmail.com>
2019-10-02 17:05:53 +0100

    Removed core reference

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const child_process = require('child_process');
 
 try {
-    child_process.execSync('docker pull mergermarket/cdflow-commands:latest')
-    child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py | chmod +x cdflow')
+    child_process.execSync('docker pull mergermarket/cdflow-commands:latest');
+    child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py | chmod +x cdflow');
     //core.exportVariable('CDFLOW_IMAGE_ID','mergermarket/cdflow-commands')
 }catch {
 
