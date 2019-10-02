c2cef2d1df42abd8b8cd6d8c316e40c38679772b
Marcio Goda <marciogoda@gmail.com>
2019-10-02 16:42:57 +0100

    Moved to docker pull

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const core = require('@actions/core');
 const child_process = require('child_process');
 
 try {
-
-    child_process.execSync('pip3 install --user --upgrade setuptools wheel | sudo pip3 install cdflow')
-
+    child_process.execSync('docker pull mergermarket/cdflow-commands:latest')
+    child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py | chmod +x cdflow')
+    core.exportVariable('CDFLOW_IMAGE_ID','mergermarket/cdflow-commands')
 }catch {
 
 }
