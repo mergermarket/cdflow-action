06681bd0648b823d2ae85eb53ab5cce6a92edd39
Kintaro-Oe <marcusclairmont@gmail.com>
2019-10-02 17:53:46 +0100

    add dependencies

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const child_process = require('child_process');
 
 try {
-    child_process.execSync('docker pull mergermarket/cdflow-commands:latest');
-    child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py');
-    child_process.execSync('chmod +x ./cdflow ')
-    child_process.execSync('sudo mv ./cdflow /usr/local/bin/cdflow')
-    //core.exportVariable('CDFLOW_IMAGE_ID','mergermarket/cdflow-commands')
-}catch {
+  child_process.execSync('docker pull mergermarket/cdflow-commands:latest');
+  child_process.execSync('curl -o cdflow https://raw.githubusercontent.com/mergermarket/cdflow/master/cdflow.py');
+  child_process.execSync('chmod +x ./cdflow ')
+  child_process.execSync('sudo mv ./cdflow /usr/local/bin/cdflow')
+  child_process.execSync('sudo pip install docker boto3 PyYAML dockerpty')
+  //core.exportVariable('CDFLOW_IMAGE_ID','mergermarket/cdflow-commands')
+} catch {
 
 }
